3715d8b8c4765167a57841b81d0f53f9b28a3d02
Denis <naumenkovdenis3@gmail.co>
2019-11-03 13:41:35 +0300

    init commit

---
+++ MyClientServerApplication/Assets/Resources/DataBase/DataBaseConnectionPHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataBaseConnectionPHP : MonoBehaviour, IDataBaseConnection 
+{
+    public Text t;
+    public IEnumerator Connection()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("W","S");
+        UnityWebRequest download = UnityWebRequest.Post("https://denisnvgames.000webhostapp.com/index.php", form);
+        yield return download.SendWebRequest();
+        if (download.isNetworkError || download.isHttpError)
+        {
+            print("Error downloading: " + download.error);
+        }
+        else
+        {
+            print(download.downloadHandler.text);
+        }
+    }
+    private void Start()
+    {
+        StartCoroutine(Connection());
+    }
+
+
+}
---
+++ MyClientServerApplication/Assets/Resources/DataBase/DataConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataConnection : MonoBehaviour
+{
+   DataBaseConnectionPHP _connectionPHP = new DataBaseConnectionPHP();
+    void Start()
+    {
+    }
+
+    private void Connection(IDataBaseConnection  dataBase)
+    {
+        dataBase.Connection();
+
+    }
+}
